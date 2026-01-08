34929c4fe2b3460a35be7c045d3549498f08a999
lafond <macklafonddeboumas@gmail.com>
2026-01-08 17:00:09 +0100

    Add update

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -20,7 +20,7 @@ const BlogPage = ({ loaderData }: Route.ComponentProps) => {
   console.log(posts);
   return (
     <>
-      <h1 className="text-3xl font-bold text-white mb-8">Blog Page</h1>
+      <h1 className="text-3xl font-bold text-white mb-8">Blog Page Of MACK</h1>
     </>
   );
 };
